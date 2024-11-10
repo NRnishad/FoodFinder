da42f9b977bd64c130c5dee65d346ed0044989cb
NRnishad <nrnishad106@gmail.com>
2024-11-10 12:38:04 +0530

    body added

--- app.js
+++ app.js
@@ -19,14 +19,46 @@ const Header = () => (
     </div>
   </div>
 );
+function FoodCard() {
+  return (
+    <div className="card">
+      <img
+        src="https://b.zmtcdn.com/data/pictures/chains/6/18766126/7e95a4eec835cbbe4958c7d24c54578a_featured_v2.jpg?output-format=webp"
+        alt="image"
+        className="card-image"
+      />
+      <div className="card-content">
+        <h2 className="card-title">Barbeque Nation</h2>
+        <p className="card-description">
+          North Indian, BBQ, Biryani, Street Food, Kebab, Beverages, Desserts
+        </p>{" "}
+         <p className="card-price">₹1700</p>  
+      </div>
+    </div>
+  );
+}
 
-
+function FoodCardGrid() {
+  return (
+    <div className="food-card-grid">
+      <FoodCard />
+      <FoodCard />
+      <FoodCard />
+      <FoodCard />
+      <FoodCard />
+      <FoodCard />
+      <FoodCard />
+      <FoodCard />
+    </div>
+  );
+}
 
 
 const AppLayout = () => {
           return (
                     <div className="app">
-<Header/>
+                              <Header />
+                              <FoodCardGrid/>
                     </div>
           )
 }
