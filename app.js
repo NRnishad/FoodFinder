73b8f8017ca8ab57f57d8fd687cbd560bdbb3ef5
NRnishad <nrnishad106@gmail.com>
2024-11-10 18:30:29 +0530

    before src

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ const resList = [
       "Traditional soup topped with a layer of melted cheese and croutons.",
     price: 6.99,
   },
-  {
+  { 
     id: 8,
     restaurant: "Vegan Haven",
     img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ihdieablaqorjjtyiwd9",
