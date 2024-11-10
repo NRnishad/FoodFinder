46c392af1d346396e8ad90d4e646d850751b9bdf
NRnishad <nrnishad106@gmail.com>
2024-11-10 11:34:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+const Header = () => (
+  <div className="header">
+    <div className="log-container">
+      <img
+        className="log"
+        src="https://play-lh.googleusercontent.com/R1TYyE-34iv_Rt_FsCXUeCSqcZXOOdMlNaGJ72gN1LoLltz47-ix6MFbsG0JRx4RVg"
+      ></img>
+    </div>
+  </div>
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<HeadingComponent/>)
+
