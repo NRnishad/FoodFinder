ae8a6c2f0ebe625f797b6a1f1e6633853b43540c
NRnishad <nrnishad106@gmail.com>
2024-11-10 12:22:51 +0530

    header added

--- app.js
+++ app.js
@@ -9,10 +9,28 @@ const Header = () => (
         src="https://play-lh.googleusercontent.com/R1TYyE-34iv_Rt_FsCXUeCSqcZXOOdMlNaGJ72gN1LoLltz47-ix6MFbsG0JRx4RVg"
       ></img>
     </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
   </div>
 );
 
 
+
+
+const AppLayout = () => {
+          return (
+                    <div className="app">
+<Header/>
+                    </div>
+          )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
 
