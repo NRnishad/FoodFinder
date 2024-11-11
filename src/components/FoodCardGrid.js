07e2832e9a71baca89f0a74d022bb49f6f72e53e
NRnishad <nrnishad106@gmail.com>
2024-11-11 20:06:19 +0530

    components set

--- src/components/FoodCardGrid.js
+++ src/components/FoodCardGrid.js
@@ -1,12 +1,26 @@
 import FoodCard from './FoodCard';
 import resList from '../utils/mockData'
-
+import { useState } from 'react';
 
 
 function FoodCardGrid() {
+  const [cardList, setCardList] = useState(resList)
+
   return (
     <div className="food-card-grid">
-      {resList.map((restaurant) => (
+      <div>
+        <button
+        className="filter-button"
+        onClick={() => {
+          const filteredList = resList.filter((res) => res.price > 10);
+          setCardList(filteredList);
+        }}
+      >
+        filter
+      </button>
+      </div>
+      
+      {cardList.map((restaurant) => (
         <FoodCard key={restaurant.id} resData={restaurant} />
       ))}
     </div>
