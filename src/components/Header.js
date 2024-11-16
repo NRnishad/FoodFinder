52c77ec3dedf5cd95d4162c8f191d81f990a546e
NRnishad <nrnishad106@gmail.com>
2024-11-16 17:04:15 +0530

    Link . header updated with Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,41 @@
 import { LOGO,URLIMG } from "../utils/constants";
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
 const Header = () =>{ 
   const [btnReact,setbtnReact]=useState('Login')
-  return(<div className="header">
-    <div className="log-container">
-      <img className="log" src={LOGO}></img>
+  return (
+    <div className="header">
+      <div className="log-container">
+        <img className="log" src={LOGO}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <button
+            className="log-button"
+            onClick={() => {
+              btnReact === "Login"
+                ? setbtnReact("LogOut")
+                : setbtnReact("Login");
+            }}
+          >
+            {btnReact}
+          </button>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-        <button className="log-button" onClick={()=>{
-          btnReact==="Login"?setbtnReact("LogOut"):setbtnReact("Login")
-        }}>{btnReact}</button>
-      </ul>
-    </div>
-  </div>
-)};
+  );};
 
 
 export default Header;
