ef1924f6baa36b122c38d20c22e1a5bf5b49ac2d
NRnishad <nrnishad106@gmail.com>
2024-11-19 14:48:38 +0530

    class state added

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,15 @@
-const Admin = ({name , location })=> {
+import { useState } from "react";
+
+const Admin = ({ name, location }) => {
+  const [count] = useState(0) 
+  const [count2] = useState(2)
           return (
             <div className="card">
               <div className="card-content">
-                                        <h2 className="title">Name : { name}</h2>
-                                        <p className="card-price">Location : { location}</p>  
+                <h1>{count }</h1>
+                <h1>{ count2}</h1>
+                <h2 className="title">Name : {name}</h2>
+                <p className="card-price">Location : {location}</p>  
               </div>
             </div>
           );
--- src/components/AdminClass.js
+++ src/components/AdminClass.js
@@ -1,15 +1,23 @@
 import React from "react";
 class AdminClass extends React.Component{
           constructor(props) {
-                    super(props)
+            super(props)
+            this.state ={
+              count: 0,
+              count2: 2,
+            }
+            
           }
           render() {
-                    const {name, location } = this.props
+            const { name, location } = this.props
+            const {count, count2} = this.state
                 return (
                   <div className="card">
                     <div className="card-content">
-                                              <h2 className="title">Name : {name}</h2>
-                                              <p className="card-price">Location :  { location}</p>  
+                      <h1>{ count}</h1>
+                      <h1>{ count2}</h1>
+                      <h2 className="title">Name : {name}</h2>
+                      <p className="card-price">Location : {location}</p>  
                     </div>
                   </div>
                 );
