a19ef82ffc089a55e6e6ee7940a11b03db7b6468
NRnishad <nrnishad106@gmail.com>
2024-11-21 14:39:55 +0530

    fetch data with api using class

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 const Admin = ({ name, location }) => {
   const [count] = useState(0) 
-  const [count2] = useState(2)
+  
           return (
             <div className="card">
               <div className="card-content">
                 <h1>{count }</h1>
-                <h1>{ count2}</h1>
+                
                 <h2 className="title">Name : {name}</h2>
                 <p className="card-price">Location : {location}</p>  
               </div>
--- src/components/AdminClass.js
+++ src/components/AdminClass.js
@@ -1,22 +1,41 @@
 import React from "react";
 class AdminClass extends React.Component{
           constructor(props) {
-            super(props)
-            this.state ={
+            super(props);
+            this.state = {
               count: 0,
               count2: 2,
-            }
+              Api :{},
+            };
             
-          }
+
+            
+  }
+  
+  async componentDidMount() {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    const json = await data.json()
+    console.log(json)
+    this.setState({ Api: json });
+  }
           render() {
             const { name, location } = this.props
-            const {count, count2} = this.state
+            const {count,Api} = this.state
                 return (
                   <div className="card">
                     <div className="card-content">
-                      <h1>{ count}</h1>
-                      <h1>{ count2}</h1>
+                      <h1>{count}</h1>
+                      <button
+                        onClick={() => {
+                          this.setState({
+                            count: this.state.count + 1,
+                          });
+                        }}
+                      >
+                        count
+                      </button>
                       <h2 className="title">Name : {name}</h2>
+                      <h2 className="title">Name from api : {Api.name}</h2>
                       <p className="card-price">Location : {location}</p>  
                     </div>
                   </div>
